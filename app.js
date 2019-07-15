de58431ff8e42f5eda516c854d03ced54207e9be
mariiakobzar <mariia.kobzar@gmail.com>
2019-07-15 17:26:10 +0300

    Init

---
+++ app.js
@@ -0,0 +1,27 @@
+var addBtn = document.getElementById('add');
+
+addBtn.addEventListener("click", function (e) {
+  var li = document.createElement("label");
+  var text = document.getElementById("text").value;
+  if (!text) {
+    alert('Empty input!');
+    return;
+  }
+  var ll = "<input type='checkbox' id='pickl' name='pickles' value='pickles'><span>" + text + "</span><button>Delete</button>";
+  li.innerHTML = ll;
+  document.getElementById("shopingList").appendChild(li);
+})
+
+var labels = document.querySelectorAll('#shopingList label');
+
+for (var i = 0; i < labels.length; i++) {
+  var labell = labels[i];
+  labell.onclick = function (event) {
+    var target = event.target;
+    if (target.tagName != 'BUTTON') return;
+    this.remove();
+  };
+};
+
+
+
